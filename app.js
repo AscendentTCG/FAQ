aed73a8be1c3383c4627d89c7d63343d341e38af
AscendentTCG <generalkogtog@gmail.com>
2025-04-05 23:35:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const { createClient } = supabase;
+
 const SUPABASE_URL = "https://iwwkdrqwdniigeeogwie.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3d2tkcnF3ZG5paWdlZW9nd2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MDUzODMsImV4cCI6MjA1NjA4MTM4M30.K_cPRK6eksBxUFR5lqclNByn7Ia2IEoq7w46HPaxwPg";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // DOM elements
 const searchInput = document.getElementById("searchInput");
@@ -15,7 +17,7 @@ searchInput.addEventListener("input", async () => {
     return;
   }
 
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from("cards")
     .select("*")
     .ilike("name", `%${query}%`);
