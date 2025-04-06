7c30205f80a2ad520ee50381ebd926d326625533
AscendentTCG <generalkogtog@gmail.com>
2025-04-05 23:27:20 -0700

    Update app.js
    added anon key and url from supabase.
    managed Row level security on supabase beforehand.
    only using anon key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://your-project.supabase.co";
-const SUPABASE_KEY = "your-anon-key";
+const SUPABASE_URL = "https://iwwkdrqwdniigeeogwie.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3d2tkcnF3ZG5paWdlZW9nd2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MDUzODMsImV4cCI6MjA1NjA4MTM4M30.K_cPRK6eksBxUFR5lqclNByn7Ia2IEoq7w46HPaxwPg";
 
 async function searchCards() {
   const query = document.getElementById("searchInput").value;
