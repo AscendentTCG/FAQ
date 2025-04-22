6791c0bec8ded60a71063c025f71547b48022472
AscendentTCG <generalkogtog@gmail.com>
2025-04-21 18:21:37 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://iwwkdrqwdniigeeogwie.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3d2tkcnF3ZG5paWdlZW9nd2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MDUzODMsImV4cCI6MjA1NjA4MTM4M30.K_cPRK6eksBxUFR5lqclNByn7Ia2IEoq7w46HPaxwPg";
-
+const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
